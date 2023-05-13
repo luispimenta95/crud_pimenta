04a2fa800377df6c14b13cf1a5add9b8a2e4c0e4
Luis Felipe <luisfelipearaujopimenta@gmail.com>
2023-05-13 16:10:26 -0300

    projeto finalizado

--- app/src/main/java/com/example/crudpimenta/bd/Database.java
+++ app/src/main/java/com/example/crudpimenta/bd/Database.java
@@ -22,6 +22,7 @@ public class Database extends SQLiteOpenHelper {
     public static final String COLUNA_NOME = "usuario_nome";
     public static final String COLUNA_CPF = "usuario_cpf";
     public static final String COLUNA_TELEFONE = "usuario_telefone";
+    public static final String ORDENACAO = " ORDER BY " + COLUNA_NOME +" ASC"; // ORDENA POR ORDEM ALFABÉTICA
     private Context context;
 
     public Database(@Nullable Context context) {
@@ -29,24 +30,10 @@ public class Database extends SQLiteOpenHelper {
         this.context = context;
     }
 
-    //truy vấn không trả về kết quả: CREATE, INSERT, UPDATE, DELETE...
-    public void QueryData(String sql){
-        SQLiteDatabase database = getWritableDatabase();
-        //để thực thi lệnh cần gọi
-        database.execSQL(sql);
-    }
 
-    //truy vấn có trả về kết quả: SELECT
-    //dữ liệu trả về dưới dạng con trỏ từng dòng (cursor)
-    public Cursor GetData(String sql){
-        SQLiteDatabase database = getReadableDatabase();
-        return database.rawQuery(sql, null);
-    }
-
-    //Tìm kiếm bằng query
     public Cursor pesquisaPorNome(String text){
         SQLiteDatabase db = this.getReadableDatabase();
-        String query = "SELECT * FROM " + NOME_TABELA + " WHERE " + COLUNA_NOME + " Like '%"+ text +"%'";
+        String query = "SELECT * FROM " + NOME_TABELA + " WHERE " + COLUNA_NOME + " Like '%"+ text +"%'" +ORDENACAO;
         Cursor cursor = db.rawQuery(query, null);
         return cursor;
     }
@@ -132,9 +119,9 @@ public class Database extends SQLiteOpenHelper {
     public List<Usuario> recuperarUsuarios(){
         List<Usuario> returnList = new ArrayList<Usuario>();
         //get data from database
-        String selectQuery = "SELECT * FROM " + NOME_TABELA;
+        String selectQuery = "SELECT * FROM " + NOME_TABELA + ORDENACAO;
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
+        Cursor cursor = db.rawQuery(selectQuery,null);
         if(cursor.moveToFirst()){
             do{
                 Usuario user = new Usuario();
@@ -160,7 +147,7 @@ public class Database extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(NOME_TABELA, new String[] {COLUNA_ID, COLUNA_NOME,COLUNA_CPF,COLUNA_TELEFONE},
                 COLUNA_CPF + "=?",new String[] { String.valueOf(text) },
-                null, null, null, null);
+                null, null, "desc", null);
         if (cursor != null) {
             cursor.moveToFirst();
         }
--- app/src/main/java/com/example/crudpimenta/readme.md
+++ app/src/main/java/com/example/crudpimenta/readme.md
@@ -7,8 +7,13 @@
             <li>Linguagem : Java</li>
             <li>IDE : AndroidStudio</li>
             <li>Adicional 1 : Filtro de pesquisa por nome</li>
-            <li>Adicional 2 : Valida registros previamente registrados evitando duplicidade (Foi usado o CPF para garantir um registro por pessoa)</li>
-            <li>Adicional 3 : Além de evitar duplicidade com base no CPF, verifica se o mesmo é válido</li>
+            <li>Adicional 2 : Tanto a pesquisa inicial, quanto a que usa filtro, estão ordenadas alfabeticamente</li>
+            <li>Adicional 3 : Valida registros previamente registrados evitando duplicidade (Foi usado o CPF para garantir um registro por pessoa)</li>
+            <li>Adicional 4 : Além de evitar duplicidade com base no CPF, verifica se o mesmo é válido</li>
+            <li>Adicional 5 : Os campos CPF e Telefone estão devidamente formatados ao visualizar um usuário</li>
+            <li>Adicional 6: Para questões de aprendizado, o aplicativo não permite edição do campo CPF ( na edição o campo é do tipo somente leitura) </li>            
+            <li>Adicional 7: Para questões de aprendizado, o aplicativo teve suas activities travadas no alinhamento PORTAIT ( Horizontal) </li>
+
             
 
 </ul>
